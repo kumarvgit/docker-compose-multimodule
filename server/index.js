340c21e008a941d0314a974dc92853b9ead050f0
kumarvgit <vaibhav.cs.kumar@gmail.com>
2020-12-24 13:50:30 +0000

    corrected and carried from stephen

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const keys = require('./keys');
 
-// Express app setup
+// Express App Setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -9,64 +9,63 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgress client setup
-const { Pool } = new Pool({
-    user: keys.pgUser,
-    host: keys.pgHost,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
+// Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
 });
 
-// create table
 pgClient.on('connect', () => {
-    pgClient
-        .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-        .catch((err) => console.log(err));
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch((err) => console.log(err));
 });
 
-// Redis client setup
-
-const redis = require(redis);
-const redisClient = redis.redisClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const redisPublisher = redisClient.duplicate();
 
-// Express test route
+// Express route handlers
+
 app.get('/', (req, res) => {
-    res.send('Hi');
+  res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * from values');
-    res.send(values.rows);
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
-    redisClient.hgetall('values', (err, values) => {
-        res.send(values);
-    });
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
 });
 
 app.post('/values', async (req, res) => {
-    const index = req.body.index;
-    if (parseInt(index > 40)) {
-        res.status(422).send('Value too high');
-    }
+  const index = req.body.index;
 
-    redisClient.hset('values', index, 'Nothing yet!');
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
 
-    redisPublisher.publish('insert', index);
-    pgClient.query('INSERT into values(number) VALUES ($1)', [index]);
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    res.send({ working: true });
+  res.send({ working: true });
 });
 
-
-app.listen(5000, err => {
-    console.log('Listening');
+app.listen(5000, (err) => {
+  console.log('Listening');
 });
--- worker/index.js
+++ worker/index.js
@@ -2,31 +2,18 @@ const keys = require('./keys');
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.port,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const sub = redisClient.duplicate();
 
-/**
- *  
- * calculate fibonaci series
- * @param {Integer as index of fibonaci series} index 
- */
 function fib(index) {
-    if (index < 2) return 1;
-    return fib(index - 1) + fin(index - 2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-/**
- * here messagew is the index in fibonaci series
- */
 sub.on('message', (channel, message) => {
-    redisClient.hset('values', message, fib(parseInt(message)));
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-
-/**
- * Subscribe to insert event
- */
 sub.subscribe('insert');
